1b76b8a5b147c28f274df756d6164922ce9d582b
Dane Naebers <danenaebers@gmail.com>
2016-12-14 17:34:24 +0100

    Building the User class

--- SparkyTheSmartClock/Classes/User.cs
+++ SparkyTheSmartClock/Classes/User.cs
@@ -2,11 +2,103 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SparkyTheSmartClock
 {
     class User
     {
+
+        //fields
+
+        //user info
+        string firstname;
+        string surname;
+        string school;
+
+        //user adres
+        string streetName;
+        int houseNumber;
+        int postcodeNumber;
+        string postcodeLetters;
+
+        //properties
+        public string Firstname
+        {
+            get { return firstname; }
+            set
+            {
+                if (Regex.IsMatch(value, @"^[a-zA-Z]+$"))
+                {
+                    firstname = value;
+                }
+                else { }
+            }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (Regex.IsMatch(value, @"^[a-zA-Z]+$"))
+                {
+                    surname = value;
+                }
+                else { }
+            }
+        }
+
+        public string School { set; get; }
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+        public int PostcodeNumber
+        {
+            get { return postcodeNumber; }
+            set
+            {
+                if (value.ToString().Length == 4)
+                {
+                    postcodeNumber = value;
+                }
+                else { }
+            }
+        }
+
+        public string PostcodeLetters
+        {
+            get { return postcodeLetters; }
+            set
+            {
+                if (Regex.IsMatch(value, @"^[A-Z]+$") && value.Length == 2)
+                {
+                    postcodeLetters = value;
+                }
+                else { }
+            }
+        }
+
+        public string Adres
+        {
+            get
+            {
+                return postcodeLetters + postcodeNumber + houseNumber;
+            }
+        }
+
+        //functions
+
+
+
+
+        //constructors
+
+        public User(string firstname, string surname, string school)
+        {
+            this.firstname = firstname;
+            this.surname = surname;
+            this.school = school;
+        }
     }
 }
