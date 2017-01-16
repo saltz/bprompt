cd2182a6928e1ed1db584299b9945d144d267f12
Dane Naebers <danenaebers@gmail.com>
2017-01-16 09:05:50 +0100

    Finishing touch

--- SparkyTheSmartClock/main.Designer.cs
+++ SparkyTheSmartClock/main.Designer.cs
@@ -554,6 +554,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(360, 604);
             this.Controls.Add(this.menuNav);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(376, 643);
             this.Name = "main";
             this.Text = "Sparky App";
